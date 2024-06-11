fc4306b183afb0d0c70930dd9af951217e528838
alexbd123 <alexpjbd@gmail.com>
2024-06-11 16:20:46 +0100

    Change image address to web

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ export default function Title() {
     return (
         <div>
             <h1 className="title">Sofiane Pamart</h1>
-            <img className="img" src="src/assets/sofiane-pamart.jpg"/>
+            <img className="img" src="https://images.genius.com/a032c75a67e5cc57e0a3c476051c0560.1000x1000x1.jpg"/>
         </div>
     )
 }
