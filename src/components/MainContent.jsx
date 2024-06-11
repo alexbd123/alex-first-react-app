1bc449cc15175fe839e35d5c09d4f1a42e4e0ebd
alexbd123 <alexpjbd@gmail.com>
2024-06-11 16:08:53 +0100

    trying to fix image

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,12 +5,12 @@ export default function MainContent() {
         <div className="mainContent">
             <h2 className="subtitle">Who is Sofiane Pamart?</h2>
             <p className="info">
-Sofiane Pamart is a gifted French pianist and composer renowned for his evocative and emotive musical creations.
-With a distinctive style blending classical, jazz, and contemporary influences, Pamart captivates audiences with his virtuosity and heartfelt performances.
-His compositions often reflect a profound sensitivity, exploring themes of introspection, love, and human connection.
-Pamart's music transcends boundaries, offering listeners an immersive journey through soundscapes that resonate deeply with the soul.
-Whether performing solo or collaborating with other artists, he consistently delivers music that enchants and inspires.
-Explore the enchanting world of Sofiane Pamart and experience the magic of his artistry.
+            Sofiane Pamart is a gifted French pianist and composer renowned for his evocative and emotive musical creations.
+            With a distinctive style blending classical, jazz, and contemporary influences, Pamart captivates audiences with his virtuosity and heartfelt performances.
+            His compositions often reflect a profound sensitivity, exploring themes of introspection, love, and human connection.
+            Pamart's music transcends boundaries, offering listeners an immersive journey through soundscapes that resonate deeply with the soul.
+            Whether performing solo or collaborating with other artists, he consistently delivers music that enchants and inspires.
+            Explore the enchanting world of Sofiane Pamart and experience the magic of his artistry.
             </p>
             <h3 className="songs">What are his best pieces?</h3>
             <ul className="list">
